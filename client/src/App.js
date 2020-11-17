ac173f9ef62485a25a6abe95c9867243eac00532
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2020-11-18 02:18:51 +0900

    til_lecture_9

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root: {
@@ -17,14 +18,20 @@ const styles = theme => ({
   },
   table:{
     minWidth:1080
+  },
+  progress : {
+    margin: theme.spacing * 2
   }
 })
+
 class App extends Component{
   state={
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -34,6 +41,11 @@ class App extends Component{
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () =>{
+    const { completed } = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed+1});
   }
 
   render(){
@@ -65,7 +77,13 @@ class App extends Component{
                       img = {c.img}
                       />
               );
-            }) : ""}
+            }) : 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
           </Table>
       </Paper>
