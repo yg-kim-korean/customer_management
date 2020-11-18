c0e84e96732db2c743c82cf669faf4a107e252e8
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2020-11-19 01:34:20 +0900

    Update UI/UX & Add Customer Search Module

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  typography:{
+    fontFamily:'"Noto Sans KR", serif'
+  }
+})
 ReactDOM.render(
   
-    <App />
+    <MuiThemeProvider theme={theme}><App /></MuiThemeProvider>
   ,
   document.getElementById('root')
 );
